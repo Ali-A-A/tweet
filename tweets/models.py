6e6c129649ba840b9cb38f60ccc319949a9d2b57
ali <aliabbasi806@gmail.com>
2020-09-10 18:40:09 +0430

    add like field to tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -3,10 +3,24 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet" , on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     user = models.ForeignKey(User , on_delete=models.CASCADE)
     content = models.TextField(blank=True , null=True)
     image = models.FileField(upload_to='images/' , blank=True , null=True)
+    likes = models.ManyToManyField(User , related_name="tweet_user" , blank=True , through=TweetLike)
+    time = models.DateTimeField(auto_now_add=True)
+
+
+# tweet.likes.add(user)
+# tweet.likes.remove(user)
+# tweet.likes.set(users)
+# TweetLike.objects.create(user= , tweet=)
+
 
     
 
--- tweets/views.py
+++ tweets/views.py
@@ -4,19 +4,23 @@ from django.http import HttpResponse , Http404 , JsonResponse
 from .models import Tweet
 import random
 from .forms import TweetForm
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
 from .serializers import TweetSerializer
+from rest_framework.decorators import api_view , authentication_classes , permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 def home_view(request):
     return render(request , 'pages/home.html' , {})
 
 
-def tweet_list_view(request):
+def tweet_list_view_pure(request):
     qs = Tweet.objects.all()
     data = {"response" : [x.serialize() for x in qs]}
     return JsonResponse(data)
 
 
-def tweet_detail(request , id):
+def tweet_detail_pure(request , id):
 
     data = {
         "id" : id,
@@ -32,13 +36,47 @@ def tweet_detail(request , id):
     
     return JsonResponse(data , status=status)
 
+@api_view(['POST'])
+# @authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def tweet_create(request):
-    serializer = TweetSerializer(data = request.POST or None)
-    if serializer.is_valid():
+    serializer = TweetSerializer(data = request.POST)
+    if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user)
-        return JsonResponse(serializer.data , status=201)
+        return Response(serializer.data , status=201)
         
-    return JsonResponse({} , status=400)
+    return Response({} , status=400)
+
+@api_view(['GET'])
+def tweet_list_view(request):
+    qs = Tweet.objects.all()
+    serializer = TweetSerializer(qs , many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def tweet_detail(request , id):
+    obj = Tweet.objects.filter(id=id)
+    if not obj.exists():
+        return Response({} , status=404)
+
+    serializer = TweetSerializer(obj[0])
+    return Response(serializer.data)
+
+@api_view(['DELETE' , 'POST'])
+@permission_classes([IsAuthenticated])
+def tweet_delete(request , id):
+    obj = Tweet.objects.filter(id=id)
+    if not obj.exists():
+        return Response({} , status=404)
+
+    qs = obj.filter(user=request.user)
+    if not qs.exists():
+        return Response({"message" : "You don't have permission!"} , stauts=403)
+    x = obj[0]
+    x.delete()
+    return Response({"message" : "Tweet Deleted Successfully"})
+
 
 
 def tweet_create_pure(reqeust):
